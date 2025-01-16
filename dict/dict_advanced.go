4a258789ecc61e1bfdbadec7abfb991290c8872d
Reiner Huechting <dev@reinerhuechting.de>
2025-01-16 21:45:30 +0100

    Remove solutions.

--- dict/dict_advanced.go
+++ dict/dict_advanced.go
@@ -1,11 +1,5 @@
 package dict
 
-import (
-	"dictionary/entry"
-	"os"
-	"strings"
-)
-
 // FromWordPairsCsv erzeugt ein neues Wörterbuch aus einer Liste von Wortpaaren.
 // Ein Wortpaar ist dabei jeweils gegeben als ein String mit zwei Wörtern,
 // getrennt durch ein Komma.
@@ -14,12 +8,7 @@ func FromWordPairsCsv(words []string) Dict {
 	// Hinweis: Laufen Sie über words und erzeugen Sie für jedes Wortpaar ein Entry-Objekt.
 	// Verwenden Sie dabei den Konstruktor entry.FromWordPairCsv().
 	d := New()
-	for _, w := range words {
-		e := entry.FromWordPairCsv(w)
-		if e.IsValid() {
-			d.Add(e)
-		}
-	}
+	// TODO
 	return d
 }
 
@@ -30,11 +19,8 @@ func ReadFileLines(filename string) []string {
 	// Beachten Sie, dass Sie dabei eine Fehlerbehandlung benötigen.
 	// Die Funktion os.ReadFile() liefert zwei Werte zurück.
 	// Verwenden Sie strings.Split(), um den Inhalt der Datei in Zeilen zu teilen.
-	file, err := os.ReadFile(filename)
-	if err != nil {
-		return []string{}
-	}
-	return strings.Split(string(file), "\n")
+	// TODO
+	return []string{}
 }
 
 // FromFile erzeugt ein neues Wörterbuch aus dem Inhalt einer Datei.
@@ -46,6 +32,6 @@ func ReadFileLines(filename string) []string {
 func FromFile(filename string) Dict {
 	// Hinweis: Lesen Sie den Inhalt der Datei mit ReadFileLines() ein.
 	// Verwenden Sie dann FromWordPairsCsv(), um ein Wörterbuch zu erzeugen.
-	lines := ReadFileLines(filename)
-	return FromWordPairsCsv(lines)
+	// TODO
+	return New()
 }
